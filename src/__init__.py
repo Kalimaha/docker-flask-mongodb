55daba5b9dbbe0495c6dc16eda08db1e60f08a4c
Kalimaha <guido.barbaglia@gmail.com>
2015-06-29 14:18:03 +0200

    Docker/Flask/CherryPy setup.

--- src/__init__.py
+++
@@ -1,4 +0,0 @@
-__package__ = 'src'
-__author__ = 'Barbaglia, Guido'
-__email__ = 'guido.barbaglia@gmail.com'
-__license__ = 'GPL2'
--- src/core/__init__.py
+++
@@ -1,4 +0,0 @@
-__package__ = 'core'
-__author__ = 'Barbaglia, Guido'
-__email__ = 'guido.barbaglia@gmail.com'
-__license__ = 'GPL2'
