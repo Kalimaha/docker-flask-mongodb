c3a3591331e5658a5d4b8515beff00472d4fa3e0
Kalimaha <guido.barbaglia@gmail.com>
2015-07-01 14:33:52 +0200

    REST Engine.

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,25 @@
+# Create a container from Ubuntu.
 FROM ubuntu:14.04
+
+# Credits.
 MAINTAINER Guido Barbaglia "guido.barbaglia@gmail.com"
 
+# Update Ubuntu repositories.
 RUN apt-get update
+
+# Install Python.
 RUN apt-get install -y python python-pip wget
-RUN apt-get install unzip
+
+# Create a working directory.
 RUN mkdir geobricks
+
+# Add requirements file.
 ADD requirements.txt /geobricks/requirements.txt
+ADD geobricks_common_settings.py /geobricks/geobricks_common_settings.py
+ADD geobricks_rest_settings.py /geobricks/geobricks_rest_settings.py
+
+# Add the script that will start everything.
 ADD start.py /geobricks/start.py
+
+# Install Python requirements.
 RUN pip install -r /geobricks/requirements.txt
-# RUN wget https://github.com/Kalimaha/docker-flask-mongodb/archive/no_cherrypy.zip
-# RUN unzip no_cherrypy.zip
--- start.py
+++ start.py
@@ -1,4 +1,19 @@
-from geobricks_flask_archetype.rest.dummy_rest_engine import start_dummy_rest_engine
+from geobricks_rest_engine.config.common_settings import settings as common_settings
+from geobricks_rest_engine.config.rest_settings import settings as rest_settings
+from geobricks_rest_engine.core.utils import dict_merge
+from geobricks_rest_engine.rest import engine as rest_engine
+from geobricks_common_settings import settings_app
+from geobricks_rest_settings import settings_rest_modules
+
+#from geobricks_common.rest import common_rest
+
+# loading settings
+common_settings["settings"] = dict_merge(common_settings, settings_app)
+common_settings["settings"] = common_settings["settings"]["settings"]
+
+# loading  modules
+rest_settings["settings"] = dict_merge(rest_settings, settings_rest_modules)
+rest_settings["settings"] = rest_settings["settings"]["settings"]
 
 
-start_dummy_rest_engine()
+rest_engine.run_engine()
