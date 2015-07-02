6bf5a1e5835870235435fdc748ac4826d38cae78
simonefenix <simone.murzilli@fao.org>
2015-07-02 18:29:35 +0200

    added just for testing

--- geobricks_common_settings.py
+++ geobricks_common_settings.py
@@ -5,7 +5,7 @@ settings_app = {
     "settings": {
 
         # base url used by nginx
-        "base_url" : "demo/fenix/geo/",
+        "base_url": "",
 
         # Logging configurations
         "logging": {
@@ -16,73 +16,12 @@ settings_app = {
 
         # Folders
         "folders": {
-            "tmp": "/work/fenix_gis/dbms/layers/tmp/",
-            "geoserver_datadir": "/geobricks/data/geoserver_data_dir/",
-            "distribution": "/work/fenix_gis/dbms/geobricks/demo/fenix/distribution/",
-            "distribution_sld": "/work/fenix_gis/dbms/geobricks/demo/fenix/distribution_sld/",
-            "storage": "/geobricks/data/storage/",
+            "tmp": "/tmp/",
+            "geoserver_datadir": "/geoserver_data_dir/",
+            "distribution": "/distribution/",
+            "distribution_sld": "/distribution_sld/",
+            "storage": "/storage/",
             "workspace_layer_separator": ":"
-        },
-
-        "db": {
-            # Spatial Database
-            "spatial": {
-                # default_db will search in the dbs["database"] as default option
-                "dbname": "fenix",
-                "host": "EXLPRFAOSTAT1.ext.fao.org",
-                "port": "5432",
-                "username": "fenix",
-                "password": "Qwaszx",
-                "schema": "spatial",
-                "tables": {
-                    "country": {
-                        "table":"gaul0_faostat3_3857",
-                        "column": {
-                            "faostat": "faost_code",
-                            "gaul": "adm0_code",
-                            "iso2":"iso2",
-                            "iso3":"iso3",
-                            "name":"adm0_name",
-                            "geom": "geom"
-                        }
-                    }
-                }
-            },
-            # Spatial Database
-            "afo": {
-                # default_db will search in the dbs["database"] as default option
-                "dbname": "africafertilizer",
-                "host": "EXLPRFAOSTAT1.ext.fao.org",
-                "port": "5432",
-                "username": "fenix",
-                "password": "Qwaszx"
-            }
-        },
-
-        # Metadata settings
-        "metadata": {
-            "url_create_metadata": "http://fenix.fao.org/d3s_/msd/resources/metadata",
-            "url_get_metadata_uid": "http://fenix.fao.org/d3s/msd/resources/metadata/uid/<uid>",
-
-            # delete metadata
-            "url_delete_metadata": "http://fenix.fao.org/d3s/msd/resources/metadata/uid/<uid>",
-
-            # get metadata
-            "url_get_metadata": "http://fenix.fao.org/d3s/msd/resources/find",
-            "url_get_metadata_full": "http://fenix.fao.org/d3s/msd/resources/metadata/uid/<uid>?full=true&dsd=true",
-
-            # coding system
-            "url_create_coding_system": "http://fenix.fao.org/d3s/msd/resources",
-            "url_data_coding_system": "http://fenix.fao.org/d3s/msd/resources/data/uid/<uid>",
-
-            # DSD
-            "url_overwrite_dsd_rid": "http://fenix.fao.org/d3s/msd/resources/dsd"
-        },
-
-        # Email configurations
-        "email": {
-            "user": "geobrickspy@gmail.com",
-            "password": "Geobricks2014"
         }
     }
 }
